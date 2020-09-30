e23fc72bd241f874e4c6b63c0c29d6d0f3c05317
ganeball9799 <ganeball9799@mail.ru>
2020-09-30 09:35:31 +0700

    Новый комментарий

--- NoteApp/NoteAppUI/MainForm.cs
+++ NoteApp/NoteAppUI/MainForm.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// Новый комментарий
 namespace NoteAppUI
 {
     public partial class MainForm : Form
